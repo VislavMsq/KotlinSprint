770fce20f8789a013492ecbe8b6c080f79d08bd3
vladislavmosuyk <tokenvlad@gmail.com>
2025-07-31 13:01:41 +0200

    TS-3 gagarin

---
+++ src/main/kotlin/lesson_2/tusk_3.kt
@@ -0,0 +1,13 @@
+package com.example.lesson_2
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("$year, $hour:$minute")
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
