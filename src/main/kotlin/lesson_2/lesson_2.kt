d1dbfd2a34607af7c08b5305e16d74b51bad1dcf
vladislavmosuyk <tokenvlad@gmail.com>
2025-07-31 12:40:10 +0200

    add 2 veriable

---
+++ src/main/kotlin/lesson_2/lesson_2.kt
@@ -0,0 +1,10 @@
+package com.example.lesson_2
+
+import sun.security.util.Debug.println
+
+fun main() {
+    val internetOffers: Int = 75
+    val gratitude: String = "Спасибо за покупку!"
+
+    println("$internetOffers, $gratitude")
+}
