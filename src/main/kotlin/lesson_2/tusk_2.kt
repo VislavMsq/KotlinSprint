bbd6bf334c7029bbabec4a15eb2832dcf9414c57
vladislavmosuyk <tokenvlad@gmail.com>
2025-07-31 12:51:00 +0200

    TS-2 employee

---
+++ src/main/kotlin/lesson_2/tusk_2.kt
@@ -0,0 +1,15 @@
+package com.example.lesson_2
+
+fun main(){
+    val internetOffers: Int = 75
+    val gratitude: String = "Спасибо за покупку!"
+
+    println("$internetOffers, $gratitude")
+
+    var employee: Short = 2000
+    println("Текущее количество сотрудников = $employee")
+
+    employee = 1999
+    println("Текущее количество сотрудников после увольнения = $employee")
+
+}
