1f4aeff6d70af6431065af32d223c630866f42a6
vladislavmosuyk <tokenvlad@gmail.com>
2025-08-02 11:52:04 +0200

    KS-1-5 gagarin in kosmos

---
+++ src/main/kotlin/lesson_1/tusk_5.kt
@@ -0,0 +1,21 @@
+package com.example.lesson_1
+
+/*
+Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
+
+- Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
+- Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
+- Значения часов, минут и секунд отображать двумя цифрами.
+ */
+fun main() {
+    val totalSeconds = 6480
+
+    val hours = totalSeconds / 3600
+    val remainingSecondsAfterHours = totalSeconds % 3600
+    val minutes = remainingSecondsAfterHours / 60
+    val seconds = remainingSecondsAfterHours % 60
+
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println("Время полёта Гагарина: [$formattedTime]")
+}
