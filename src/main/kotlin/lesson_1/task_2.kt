03d3b89a922fd3570a7f483bd73c3c7badc5e609
vladislavmosuyk <tokenvlad@gmail.com>
2025-08-02 11:28:44 +0200

    KS-1-2 server log

---
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -0,0 +1,9 @@
+package com.example.lesson_1
+
+fun main() {
+    var numberOfEmployees = 2000
+    println("Количество работников: $numberOfEmployees")
+    
+    numberOfEmployees = 1999
+    println("После увольнения осталось работников: $numberOfEmployees")
+}
