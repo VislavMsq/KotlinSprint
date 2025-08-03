5b56adaa857541984942364ad1feed2a792c0831
vladislavmosuyk <tokenvlad@gmail.com>
2025-08-03 12:56:19 +0200

    refactor

--- src/main/kotlin/lesson_1/task_2.kt
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -1,9 +1,17 @@
 package com.example.lesson_1
 
 fun main() {
-    var numberOfEmployees = 2000
+
+    val orders: Int = 75
+    val gratitude: String = "Thank you for your order!"
+
+    println(orders)
+    println(gratitude)
+
+//    var numberOfEmployees: Int = 2000
+    // println(numberOfEmployees) // Этот вывод закомментирован по условию задачи
+
+    // Изменение количества работников
+    val numberOfEmployees = 1999
     println("Количество работников: $numberOfEmployees")
-    
-    numberOfEmployees = 1999
-    println("После увольнения осталось работников: $numberOfEmployees")
 }
